3d75f775a8206af05f7b635380c3b8f8a8613331
dvsanta <s.santaa@uniandes.edu.co>
2022-08-10 19:15:14 -0500

    Taller terminado

---
+++ src/uniandes/dpoo/taller0/modificacion/modificacion.java
@@ -0,0 +1,13 @@
+package uniandes.dpoo.taller0.modificacion;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import uniandes.dpoo.taller0.procesamiento.CalculadoraEstadisticas;
+import uniandes.dpoo.taller0.procesamiento.LoaderOlimpicos;
+public class modificacion {
+
+    public void Main () throws FileNotFoundException, IOException {
+        CalculadoraEstadisticas calc = LoaderOlimpicos.cargarArchivo("./data/atletas.csv");
+        System.out.println(calc.paisConMasMedallistas());
+    }
+}
